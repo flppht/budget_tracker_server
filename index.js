87d62ad416aa9795a74dd61fe17d548157ffac69
flppht <kulasinacfilip@gmail.com>
2024-02-26 15:37:54 +0100

    new change

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+const favicon = require("serve-favicon");
 require("dotenv").config();
 
 const db = require("./models");
