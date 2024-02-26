73f983d9bd27c9d751b8023e2cc23e2a5aa0abec
flppht <kulasinacfilip@gmail.com>
2024-02-26 15:36:17 +0100

    new change

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 
 const db = require("./models");
 
 app.use(express.json());
 app.use(cors());
+app.use(favicon(path.join(dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(dirname, "build")));
 
 //routers
 const expensesRouter = require("./routes/Expenses");
@@ -18,11 +20,13 @@ app.use("/income", incomeRouter);
 const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
 
+const port = process.env.PORT || 3001;
+
 db.sequelize
   .sync()
   .then(() => {
-    app.listen(process.env.PORT || 3001, () => {
-      console.log("Server is listening on port: ", process.env.PORT);
+    app.listen(port, () => {
+      console.log("Server is listening on port: ", port);
     });
   })
   .catch((err) => console.log(err));
