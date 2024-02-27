a75a3e090bf8717bc3ba0cd4cbd680a11fe33d95
flppht <kulasinacfilip@gmail.com>
2024-02-27 11:12:28 +0100

    try cors

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ const db = require("./models");
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-  // Change * to your specific origin if needed
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+// app.use((req, res, next) => {
+//   // Change * to your specific origin if needed
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+//   // Handle preflight requests
+//   if (req.method === "OPTIONS") {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 
 //routers
 const expensesRouter = require("./routes/Expenses");
